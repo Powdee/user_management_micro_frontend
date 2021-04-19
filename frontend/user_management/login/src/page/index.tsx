912449db09e920b1c9afa109f0d1a076603b79e3
Erik Kurjak <e.kurjak@gmail.com>
2021-04-19 18:37:42 +0200

    setup login page - wip

--- frontend/user_management/login/src/page/index.tsx
+++ frontend/user_management/login/src/page/index.tsx
@@ -5,10 +5,11 @@ const LoginRoute = () => (
 	<React.Suspense fallback={<span>loading...</span>}>
 		<Route path="/login">
 			Login <br />
-
 			<Link to="/">go back</Link>
+			<br />
+			<Link to="/signup">Sign up</Link>
 		</Route>
-	</React.Suspense>
+	</React.Suspense> 
 )
 
 export default LoginRoute;
